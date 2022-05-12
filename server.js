904c83a1a98dfd9a6c44271e1e88e84acac1b0df
Cuke7 <cuke.dev@mail.com>
2022-05-12 11:36:32 +0200

    tset

--- server.js
+++ server.js
@@ -16,8 +16,27 @@ app.get('/wiki', (req, res) => {
         if (err) {
             res.json(err)
         } else {
-            let descriptionFR = data.split("Description (fr) ------\r\n")[1]
-            res.json({ descriptionFR })
+            let description = data.split("Description (fr) ------\r\n")[1]
+
+            if (description) {
+                let regex =
+                    /@Compendium\[pf2e\.[ ]*?([A-z-0-9]*?)\.[ ]*?([A-z0-9]*?)\]\{(.*?)\}/gm;
+                let descriptionFR = description;
+                let matchs = descriptionFR.matchAll(regex);
+                for (const match of matchs) {
+                    descriptionFR = descriptionFR.replace(
+                        match[0],
+                        "<a href='/" + match[1] + "/" + match[2] + "'>" + match[3] + "</a>"
+                    );
+                }
+                res.json({ descriptionFR })
+            } else {
+                res.json({ descriptionFR: "error" })
+            }
+
+
+
+
         }
     })
 })
