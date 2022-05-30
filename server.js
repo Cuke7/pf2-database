781709c27ff358712dfd938ef8d81b8c1133f425
Cuke7 <cuke.dev@mail.com>
2022-05-30 19:19:24 +0200

    test

--- server.js
+++ server.js
@@ -12,39 +12,44 @@ app.get('/wiki', (req, res) => {
     let shortID = req.query.id
 
     let item = dictionary.find(item => item.link.split("/")[1].includes(shortID))
-    let nameFR = item.nameFR
-    let category = item.link.split("/")[0]
-    let id = item.link.split("/")[1]
+    if (item) {
+        let nameFR = item.nameFR
+        let category = item.link.split("/")[0]
+        let id = item.link.split("/")[1]
 
-    fs.readFile("PF2E_data_FR/" + category + "/" + id + ".htm", "utf8", function (err, data) {
-        if (err) {
-            res.json(err)
-        } else {
-            let description = null
-
-            if (!PROD) {
-                description = data.split("Description (fr) ------\r\n")[1] // NODE
+        fs.readFile("PF2E_data_FR/" + category + "/" + id + ".htm", "utf8", function (err, data) {
+            if (err) {
+                res.json(err)
             } else {
-                description = data.split("Description (fr) ------\n")[1] // PROD
-            }
+                let description = null
 
-            if (description) {
-                let regex =
-                    /@Compendium\[pf2e\.[ ]*?([A-z-0-9]*?)\.[ ]*?([A-z0-9]*?)\]\{(.*?)\}/gm;
-                let descriptionFR = description;
-                let matchs = descriptionFR.matchAll(regex);
-                for (const match of matchs) {
-                    descriptionFR = descriptionFR.replace(
-                        match[0],
-                        "<a href='/" + match[1] + "/" + match[2] + "'>" + match[3] + "</a>"
-                    );
+                if (!PROD) {
+                    description = data.split("Description (fr) ------\r\n")[1] // NODE
+                } else {
+                    description = data.split("Description (fr) ------\n")[1] // PROD
                 }
-                res.json({ nameFR, descriptionFR })
-            } else {
-                res.json({ descriptionFR: null })
+
+                if (description) {
+                    let regex =
+                        /@Compendium\[pf2e\.[ ]*?([A-z-0-9]*?)\.[ ]*?([A-z0-9]*?)\]\{(.*?)\}/gm;
+                    let descriptionFR = description;
+                    let matchs = descriptionFR.matchAll(regex);
+                    for (const match of matchs) {
+                        descriptionFR = descriptionFR.replace(
+                            match[0],
+                            "<a href='/" + match[1] + "/" + match[2] + "'>" + match[3] + "</a>"
+                        );
+                    }
+                    res.json({ nameFR, descriptionFR })
+                } else {
+                    res.json({ descriptionFR: null })
+                }
             }
-        }
-    })
+        })
+    } else {
+        res.json(null)
+    }
+
 })
 
 app.get('/getDataSets', (req, res) => {
